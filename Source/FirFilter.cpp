c279d3e30b2598b206aa8ebefc302043c8813778
Wegelius Alvar <wegelia1@smirgeline.org.aalto.fi>
2015-11-27 11:05:29 +0200

    Finnish variable names to English

--- Source/FirFilter.cpp
+++ Source/FirFilter.cpp
@@ -5,17 +5,17 @@ FirFilter::FirFilter() {};
 FirFilter::FirFilter(std::vector<double> coefficients) : coefficients(coefficients) {}
 
 AudioSampleBuffer FirFilter::operator() (AudioSampleBuffer& input) {
-  AudioSampleBuffer suodatettu(input.getNumChannels(), input.getNumSamples());
+  AudioSampleBuffer filtered(input.getNumChannels(), input.getNumSamples());
   for (size_t chan = 0; chan < input.getNumChannels(); chan++) {
     const float* inputData = input.getReadPointer(chan);
-    float* suodatettuData = suodatettu.getWritePointer(chan);
+    float* filteredData = filtered.getWritePointer(chan);
     for (int n = 0; n < input.getNumSamples(); n++) {
       for (int nx = 0; nx < coefficients.size(); nx++) {
 	if (n-nx < 0)
 	  break;
-	suodatettuData[n] = suodatettuData[n] + inputData[n-nx]*coefficients[nx];
+	filteredData[n] = filteredData[n] + inputData[n-nx]*coefficients[nx];
       }
     }
   }
-  return suodatettu;
+  return filtered;
 }
