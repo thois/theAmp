ca8e3212691deadee8a490074409d0868c8e6be9
Lindvall Niko <niko.lindvall@aalto.fi>
2015-11-30 15:56:23 +0200

    Initial build script. Additions to readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # theAmp
 Guitar amplifier simulator project for SODIS-course
+
+REPOSITORY STRUCTURE
+--------------------
+
+Doc/          Project documentation
+Source/       Source code
+TODO
+
+
+DEPENDENCIES
+------------
+VST
+Juce
+
+BUILD AND DOWNLOAD LIBS
+---------------------
+Linux: Run "./build.sh" to download and compile Irrlicht and compile the game
+OS X: TODO
+Windows: TODO
+
+USAGE
+-----
+After building load plugin from the platform dependent build folder
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+vst_version="vstsdk365_12_11_2015_build_67"
+
+echo "Downloading libs"
+if [ ! -d "Libs" ]; then
+	mkdir Libs
+fi
+pushd lib
+if [ ! -f ${vst_version}.zip ]; then
+	wget http://www.steinberg.net/sdk_downloads/${vst_version}.zip
+fi
+if [ ! -f juce-grapefruit-linux.zip ]; then
+	wget http://assets.roli.com/juce/juce-grapefruit-linux.zip
+fi
+echo "Unzipping libs"
+unzip -q ${vst_version}.zip
+unzip -q juce-grapefruit-linux.zip
+popd
+echo "Building the Game"
+pushd Builds/Linux
+make -j4
+popd
+popd
+echo "All done, run use \"Builds/Linux/build.theAmp.so\" as VST plugin"
